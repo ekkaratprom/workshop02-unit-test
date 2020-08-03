46bd5205957803fb6021c1e1cc520ebbf4fda909
Ekkarat Promrit <ekkaratpro@mathregit.group>
2020-08-03 15:45:17 +0700

    :heavy_plus_sign: Add demo Circular buffer

---
+++ src/test/java/CircularBufferTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CircularBufferTest {
+
+    @Test
+    public void create_new_buffer_should_empty(){
+        CircularBuffer cb = new CircularBuffer();
+        boolean result = cb.isEmpty();
+        assertTrue("Buffer not empty",result);
+
+    }
+
+    @Test
+    public void create_new_buffer_with_default_size_should_be_10(){
+        CircularBuffer cb = new CircularBuffer();
+        for (int i = 0; i < 10; i++) {
+            cb.writeData("A" + i);
+        }
+        boolean result = cb.isFull();
+        assertTrue("Buffer not Full",result);
+
+    }
+
+    @Test
+    public void write_A_to_buffer_should_read_A(){
+        CircularBuffer cb = new CircularBuffer();
+        cb.writeData("A");
+        cb.writeData("B");
+        assertEquals("A",cb.readData());
+        assertEquals("B",cb.readData());
+    }
+
+
+
+}
